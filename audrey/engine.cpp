a676824f69aacab22322d3da7a3bef37bf8db465
Svyatoslav Usachev <kaathewise@gmail.com>
2026-03-19 22:45:50 +0100

    Refactor audrey/engine: remove setters

--- audrey/engine.cpp
+++ audrey/engine.cpp
@@ -65,107 +65,34 @@ void Engine::Init(const float sample_rate) {
   fb_hpf_.SetCutoff(60.f);
 }
 
-void Engine::SetParameters(const EngineParameters &params) {
-  SetStringPitch(params.string_pitch);
-  SetFeedbackGain(params.feedback_gain);
-  SetFeedbackDelay(params.feedback_delay);
-  SetFeedbackLPFCutoff(params.feedback_lpf_cutoff);
-  SetFeedbackHPFCutoff(params.feedback_hpf_cutoff);
-  SetEchoDelayTime(params.echo_delay_time);
-  SetEchoDelayFeedback(params.echo_delay_feedback);
-  SetEchoDelaySendAmount(params.echo_delay_send_amount);
-  SetReverbMix(params.reverb_mix);
-  SetReverbFeedback(params.reverb_feedback);
-  SetOutputLevel(params.output_level);
-  SetInputLevel(params.input_level);
-  SetShape(params.shape);
-  DroneMode(params.drone_mode);
-  if (params.trigger == TriggerState::RISING_EDGE) {
-    NoteOn();
-  } else if (params.trigger == TriggerState::FALLING_EDGE) {
-    NoteOff();
-  }
-}
-
-void Engine::SetStringPitch(const float nn) {
-  const auto freq = mtof(nn);
-  strings_[0]->SetFreq(freq);
-  strings_[1]->SetFreq(freq);
-}
-
-void Engine::SetFeedbackGain(const float gain_db) {
-  fb_gain_ = dbfs2lin(gain_db);
-}
-
-void Engine::SetFeedbackDelay(const float delay_s) {
-  fb_delay_samp_target_ =
-      DSY_CLAMP(delay_s * sample_rate_, 1.0f,
-                static_cast<float>(kMaxFeedbackDelaySamp - 1));
-}
-
-void Engine::SetFeedbackLPFCutoff(const float cutoff_hz) {
-  fb_lpf_.SetCutoff(cutoff_hz);
-}
-
-void Engine::SetFeedbackHPFCutoff(const float cutoff_hz) {
-  fb_hpf_.SetCutoff(cutoff_hz);
-}
-
-void Engine::SetEchoDelayTime(const float echo_time) {
-  echo_delay_[0]->SetDelayTime(echo_time);
-  echo_delay_[1]->SetDelayTime(echo_time);
-}
-
-void Engine::SetEchoDelayFeedback(const float echo_fb) {
-  echo_delay_[0]->SetFeedback(echo_fb);
-  echo_delay_[1]->SetFeedback(echo_fb);
-}
-
-void Engine::SetEchoDelaySendAmount(const float echo_send) {
-  echo_send_ = echo_send;
-}
-
-void Engine::SetReverbMix(const float mix) {
-  verb_mix_ = fclamp(mix, 0.0f, 1.0f);
-}
-
-void Engine::SetReverbFeedback(const float time) { verb_->SetFeedback(time); }
-
-void Engine::SetOutputLevel(const float level) { output_level_ = level; }
-
-void Engine::SetInputLevel(const float level) { input_level_ = level; }
-
-void Engine::NoteOn() { _env.Trigger(); }
-
-void Engine::NoteOff() { _env.Release(); }
-
-void Engine::DroneMode(bool mode) { drone = mode; }
-
-void Engine::SetShape(const float shape) { _env.SetShape(shape); }
-
 void Engine::Process(EngineParameters params, float in, float &outL,
                      float &outR) {
-  SetParameters(params);
-
   // --- Update audio-rate-smoothed control params ---
 
-  fonepole(fb_delay_samp_, fb_delay_samp_target_, fb_delay_smooth_coef_);
+  const float fb_delay_samp_target =
+      DSY_CLAMP(params.feedback_delay * sample_rate_, 1.0f,
+                static_cast<float>(kMaxFeedbackDelaySamp - 1));
+
+  fonepole(fb_delay_samp_, fb_delay_samp_target, fb_delay_smooth_coef_);
 
   // --- Process Samples ---
 
   float inL, inR, sampL, sampR, echoL, echoR, verbL, verbR;
   const float noise_samp = noise_.Process();
-  auto env = _env.Process();
 
   // ---> Feedback Loop
 
   // Get noise + feedback output
-  inL =
-      fb_delayline_[0]->Read(fb_delay_samp_) + noise_samp + (in * input_level_);
+  inL = fb_delayline_[0]->Read(fb_delay_samp_) + noise_samp +
+        (in * params.input_level);
   inR = fb_delayline_[1]->Read(daisysp::fmax(1.0f, fb_delay_samp_ - 4.f)) +
-        noise_samp + (in * input_level_);
+        noise_samp + (in * params.input_level);
 
   // Process through KS resonator
+  const float freq = mtof(params.string_pitch);
+  strings_[0]->SetFreq(freq);
+  strings_[1]->SetFreq(freq);
+
   sampL = strings_[0]->Process(inL);
   sampR = strings_[1]->Process(inR);
 
@@ -174,34 +101,54 @@ void Engine::Process(EngineParameters params, float in, float &outL,
   sampR = overdrive_[1].Process(sampR);
 
   // Filter in feedback loop
+  fb_lpf_.SetCutoff(params.feedback_lpf_cutoff);
+  fb_hpf_.SetCutoff(params.feedback_hpf_cutoff);
+
   fb_lpf_.ProcessStereo(sampL, sampR);
   fb_hpf_.ProcessStereo(sampL, sampR);
 
   // ---> Reverb
-
+  verb_->SetFeedback(params.reverb_feedback);
   verb_->Process(sampL, sampR, &verbL, &verbR);
 
+  const float verb_mix = fclamp(params.reverb_mix, 0.0f, 1.0f);
   //       (sampL * (1.0f - verb_mix_)) + verbL * verb_mix_;
   //       sampL - sampL * verb_mix + verbL * verb_mix_;
-  sampL -= (sampL - verbL) * verb_mix_;
-  sampR -= (sampR - verbR) * verb_mix_;
+  sampL -= (sampL - verbL) * verb_mix;
+  sampR -= (sampR - verbR) * verb_mix;
 
   // ---> Resonator feedback
 
+  const float fb_gain = dbfs2lin(params.feedback_gain);
+
   // Write back into delay with attenuation
-  fb_delayline_[0]->Write(sampL * (fb_gain_ * (drone ? 1.0f : env)));
-  fb_delayline_[1]->Write(sampR * (fb_gain_ * (drone ? 1.0f : env)));
+  if (params.trigger == TriggerState::RISING_EDGE) {
+    _env.Trigger();
+  } else if (params.trigger == TriggerState::FALLING_EDGE) {
+    _env.Release();
+  }
+  _env.SetShape(params.shape);
+  const float env = _env.Process();
+
+  fb_delayline_[0]->Write(sampL * (fb_gain * (params.drone_mode ? 1.0f : env)));
+  fb_delayline_[1]->Write(sampR * (fb_gain * (params.drone_mode ? 1.0f : env)));
 
   // ---> Echo Delay
 
-  echoL = echo_delay_[0]->Process(sampL * echo_send_);
-  echoR = echo_delay_[1]->Process(sampR * echo_send_);
+  echo_delay_[0]->SetDelayTime(params.echo_delay_time);
+  echo_delay_[1]->SetDelayTime(params.echo_delay_time);
+
+  echo_delay_[0]->SetFeedback(params.echo_delay_feedback);
+  echo_delay_[1]->SetFeedback(params.echo_delay_feedback);
+
+  echoL = echo_delay_[0]->Process(sampL * params.echo_delay_send_amount);
+  echoR = echo_delay_[1]->Process(sampR * params.echo_delay_send_amount);
 
   sampL = 0.5f * (sampL + echoL);
   sampR = 0.5f * (sampR + echoR);
 
   // ---> Output
-  outL = sampL * output_level_;
-  outR = sampR * output_level_;
+  outL = sampL * params.output_level;
+  outR = sampR * params.output_level;
 }
 }  // namespace audrey
